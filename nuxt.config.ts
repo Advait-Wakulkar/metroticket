7b9e86e91bd95c4dacd302a7ae1453d10f99a690
Hrishikesh Kulkarni <hrishikeshkulkarni153@gmail.com>
2025-07-13 17:24:17 -0500

    Fixed css import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
   ],
-  css: ['~/assets/css/main.css'],
   ssr: false,
   nitro: {
     prerender: {
--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
         <div class="relative">
             <div class="w-60 h-60 rounded-full bg-white flex items-center justify-center overflow-hidden">
               <img 
-                src="/metro/logo.png" 
+                src="/logo.png" 
                 alt="Metro Transit Logo" 
                 class="w-full h-full object-contain"
               />
