b6e306a09409bf18bdeb81965d64a5c3bcc3c8ef
Hrishikesh Kulkarni <hrishikeshkulkarni153@gmail.com>
2025-07-13 18:39:14 -0500

    Initial setup with Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,14 @@
 module.exports = {
   content: [
     "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue", 
+    "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
     "./app.vue"
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['system-ui', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', 'Helvetica Neue', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
