bc6e26c71de654905273f0c45fa5b356e3307414
Hrishikesh Kulkarni <hrishikeshkulkarni153@gmail.com>
2025-07-13 17:38:23 -0500

    Fixed css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,23 +12,7 @@ export default defineNuxtConfig({
     }
   },
   app: {
-    baseURL: '/metro/',
-    head: {
-      link: [
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.googleapis.com'
-        },
-        {
-          rel: 'preconnect', 
-          href: 'https://fonts.gstatic.com',
-          crossorigin: ''
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap'
-        }
-      ]
-    }
-  }
+    baseURL: '/metro/'
+  },
+  css: ['~/assets/css/main.css']
 })
